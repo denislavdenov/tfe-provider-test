ebc77d3b2486e48d6e2b61446c7e1399618ad7ba
Denislav Denov <denislavdenov@gmail.com>
2020-03-02 13:06:51 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "randomname" {
+  source  = "app.terraform.io/ddenov/randomname/local"
+  version = "0.0.3"
+}
+
+module "randomname1" {
+  source  = "app.terraform.io/ddenov/randomname/local"
+  version = "0.0.3"
+}
+
+module "randomname2" {
+  source  = "app.terraform.io/ddenov/randomname/local"
+  version = "0.0.3"
+}
